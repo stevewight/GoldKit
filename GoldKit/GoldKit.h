a804bb2ed3444ebe9ba70ad598df557fdbe9d286
Steve Wight <stevendouglaswight@gmail.com>
2018-05-07 12:27:36 -0700

    Initial Commit

---
+++ GoldKit/GoldKit.h
@@ -0,0 +1,19 @@
+//
+//  GoldKit.h
+//  GoldKit
+//
+//  Created by Steve on 5/7/18.
+//  Copyright © 2018 Steve Wight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GoldKit.
+FOUNDATION_EXPORT double GoldKitVersionNumber;
+
+//! Project version string for GoldKit.
+FOUNDATION_EXPORT const unsigned char GoldKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoldKit/PublicHeader.h>
+
+
