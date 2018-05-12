6f43c4d99b4f53ef64466880a88438646c7959c0
Steve Wight <stevendouglaswight@gmail.com>
2018-05-12 16:29:54 -0700

    add fontName file

---
+++ GoldKit/FontName.swift
@@ -0,0 +1,14 @@
+//
+//  FontMint.swift
+//  GoldMine
+//
+//  Created by Steve on 5/6/18.
+//  Copyright © 2018 Steve Wight. All rights reserved.
+//
+
+public enum FontName: String {
+    case demiBoldCond = "AvenirNextCondensed-DemiBold"
+    case regularCond = "AvenirNextCondensed-Regular"
+    case demiBold = "AvenirNext-DemiBold"
+    case regular = "AvenirNext-Regular"
+}
